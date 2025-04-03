42aabb3d68d26600791fd9b7cf6af8137e7f43d6
german-zaracho <germanzarachodev@gmail.com>
2025-04-03 20:42:28 -0300

    project description and styles

--- src/App.tsx
+++ src/App.tsx
@@ -63,8 +63,28 @@ function App() {
           </section>
           <section className="flex flex-col">
             <div>
-              <ul>
-                <li>
+              <ul className='group'>
+                <li className='group-hover:opacity-50 hover:opacity-100 transition-opacity'>
+                  <div className="group flex flex-row m-[10px]  leading-5 font-medium text-xs text-slate-400 hover:bg-[rgba(30,41,59,0.5)] rounded-[0.375rem] hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)]">
+                    <img className='mt-5 ml-5 rounded-[5px] h-[80px] w-[240px] border-2 border-slate-200/10 transition group-hover:border-slate-200/30' src={imgs['./assets/imgs/inplot-1.png']?.default} alt="Project 1" />
+                    <div className='p-5'>
+                      <h3 className='text-[white] group-hover:text-[rgb(94,234,212,1)] text-left'>InPlot</h3>
+                      <p className='text-left'>Web app for discovering and sharing opinions on movies and series. Create and publish reviews, engage with others through comments, and personalize your profile. Explore other users’ reviews and track your own contributions. Administrators have tools to manage content, ensuring a well-organized and engaging community.</p>
+                      <TechnologiesTag tags={["PHP", "Firebase", "Tailwind"]} />
+                    </div>
+                  </div>
+                </li>
+                <li className='group-hover:opacity-50 hover:opacity-100 transition-opacity'>
+                  <div className="group flex flex-row m-[10px]  leading-5 font-medium text-xs text-slate-400 hover:bg-[rgba(30,41,59,0.5)] rounded-[0.375rem] hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)]">
+                    <img className='mt-5 ml-5 rounded-[5px] h-[80px] w-[240px] border-2 border-slate-200/10 transition group-hover:border-slate-200/30' src={imgs['./assets/imgs/inplot-1.png']?.default} alt="Project 1" />
+                    <div className='p-5'>
+                      <h3 className='text-[white] group-hover:text-[rgb(94,234,212,1)] text-left'>Project 1</h3>
+                      <p className='text-left'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae saepe sunt magnam consectetur accusantium dolor minima ipsa sapiente eveniet minus non, necessitatibus quo voluptatem hic sed eaque asperiores rerum. Hic.</p>
+                      <TechnologiesTag tags={["PHP", "Firebase", "Tailwind"]} />
+                    </div>
+                  </div>
+                </li>
+                <li className='group-hover:opacity-50 hover:opacity-100 transition-opacity'>
                   <div className="group flex flex-row m-[10px]  leading-5 font-medium text-xs text-slate-400 hover:bg-[rgba(30,41,59,0.5)] rounded-[0.375rem] hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)]">
                     <img className='mt-5 ml-5 rounded-[5px] h-[80px] w-[240px] border-2 border-slate-200/10 transition group-hover:border-slate-200/30' src={imgs['./assets/imgs/inplot-1.png']?.default} alt="Project 1" />
                     <div className='p-5'>
